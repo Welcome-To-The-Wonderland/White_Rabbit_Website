11fc1ce98dfbd1672b990177a56d322bf7124cbe
rkmaxhero <rounaqkhan12@gmail.com>
2024-08-01 15:07:14 -0400

    check if it works without local host

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,35 +1,60 @@
-'use client'
-import data from '../manga.json';
-import './home.css'
-import Link from 'next/link'
-
-//run command: npm run dev
+'use client';
+import { useEffect, useState } from 'react';
+import Link from 'next/link';
 
 export default function Home() {
-  let x = "manga-ny991307";
+  const [data, setData] = useState<any[]>([]);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await fetch('https://utkfcob983.execute-api.us-east-2.amazonaws.com/dev', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ "unique-id": "alpha_numeric_id_2" }),
+        });
+
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+
+        const result = await response.json();
+        setData(result);
+      } catch (error: any) {
+        setError(error.message);
+      }
+    }
+
+    fetchData();
+  }, []);
 
   return (
     <div>
       <h1>Look at me</h1>
-      <Link href = {{
-        pathname: `/manga/${x}`,
+      <Link href={{
+        pathname: `/manga/manga-ny991307`,
       }}> some link
       </Link>
 
+      {error && <p>Error: {error}</p>}
+
       <div className="grid">
-        {Object.entries(data).map(([key, manga]: [string, any]) => {
+        {data.map((manga: any) => {
           return (
-            <div key={key} className="card">
-              <Link href = {{
-                pathname: `/manga/${key}`,
-              }}> 
-              <img src={manga.cover_link} alt={manga.title} />
-              <h3>{manga.title}</h3>
+            <div key={manga.id} className="card">
+              <Link href={{
+                pathname: `/manga/${manga.id}`,
+              }}>
+                <img src={manga.cover_link} alt={manga.title} />
+                <h3>{manga.title}</h3>
               </Link>
             </div>
-          )
+          );
         })}
       </div>
     </div>
-  )
+  );
 }
