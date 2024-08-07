6eed35a3489cc1bc07c2df271c5bd4126206cfe2
AshJaffer <167241251+AshJaffer@users.noreply.github.com>
2024-08-07 18:10:05 -0400

    added support for ionicons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
+import Script from 'next/script'
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -16,6 +18,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
+      <Script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></Script>
+      <Script noModule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></Script>
+
     </html>
   );
 }
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,13 +1,20 @@
-import React from "react"
-import styles from './header.css'
+import React from "react";
+import styles from './header.css';
+
+
 export default function Header() {
     return (
-        <div className="testContainer">
-            <h1 className="bullshit_name">MANGA WAVE</h1>
-            <p>The best platform for reading manga without ads</p>
-            <img src="/logo.svg" alt="Logo"/>
+        <div className="container">
+            <h1 className="title">MANGA WAVE</h1>
+            <p>The best platform for reading manga without ads.</p>
+            <ion-icon name="menu-outline"></ion-icon>
+            <div className="orientation">
+                <img src="/logo.svg" alt="Logo" /> 
+            </div>
             <p>Created by Ashhad Jaffer and Rounaq Khan</p>
-            <img src="https://pbs.twimg.com/media/E0MD7NbWUAQe0G-.jpg" alt="Gojo Satoru" style={{ display: 'block', margin: '0 auto' }} />
+            <img src="https://pbs.twimg.com/media/E0MD7NbWUAQe0G-.jpg" alt="Gojo Satoru" />
+            
+
         </div>
     )
 }
