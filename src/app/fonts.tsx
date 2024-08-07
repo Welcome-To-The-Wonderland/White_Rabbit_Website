64282bc5fac44f3b7014ac3954669a3f4c1deaf9
AshJaffer <167241251+AshJaffer@users.noreply.github.com>
2024-08-07 16:12:16 -0400

    synchronize

--- src/app/fonts.tsx
+++
@@ -1,7 +0,0 @@
-@import url('https://fonts.googleapis.com/css2?family=Bayon&display=swap');
-
-h1 {
-    font-family: 'Bayon', sans-serif;
-    font-weight: bold;
-    color: white; /* Base color for text */
-    -webkit-text-stroke: 2px purple; /* Purple stroke */
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,17 +1,12 @@
 import React from "react"
-import '../app/fonts' // Correct path
-
+import styles from './header.css'
 export default function Header() {
     return (
-        <div style={{ backgroundColor: '#d3d3d3', textAlign: 'center', padding: '20px' }}>
-            <h1>
-                MANGA WAVE
-            </h1>
-            <p style={{ fontFamily: 'Georgia, serif' }}>
-                The best platform for reading manga without ads, all the manga you need to cater to your weebish and perverted desires
-            </p>
-            <img src="/logo_colored.jpg" alt="Logo" />
-            <p style={{ fontFamily: 'Courier New, monospace' }}>Created by Ashhad Jaffer and Rounaq Khan</p>
+        <div className="testContainer">
+            <h1 className="bullshit_name">MANGA WAVE</h1>
+            <p>The best platform for reading manga without ads</p>
+            <img src="/logo.svg" alt="Logo"/>
+            <p>Created by Ashhad Jaffer and Rounaq Khan</p>
             <img src="https://pbs.twimg.com/media/E0MD7NbWUAQe0G-.jpg" alt="Gojo Satoru" style={{ display: 'block', margin: '0 auto' }} />
         </div>
     )
