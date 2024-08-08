58934e0a8c156908b1f275e0d79dffad62a56441
AshJaffer <167241251+AshJaffer@users.noreply.github.com>
2024-08-08 16:45:57 -0400

    Header complete :}

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -5,16 +5,8 @@ import styles from './header.css';
 export default function Header() {
     return (
         <div className="container">
-            <h1 className="title">MANGA WAVE</h1>
-            <p>The best platform for reading manga without ads.</p>
             <ion-icon name="menu-outline"></ion-icon>
-            <div className="orientation">
-                <img src="/logo.svg" alt="Logo" /> 
-            </div>
-            <p>Created by Ashhad Jaffer and Rounaq Khan</p>
-            <img src="https://pbs.twimg.com/media/E0MD7NbWUAQe0G-.jpg" alt="Gojo Satoru" />
-            
-
+            <img className="headerLogo" src="/logo.svg" alt="Logo" /> 
         </div>
     )
 }
